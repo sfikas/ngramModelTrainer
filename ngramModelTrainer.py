54113a30ad4f362db375be403ab9196f47dc2bee
Giorgos Sfikas <georges.sfikas@gmail.com>
2018-10-23 22:52:22 +0300

    Wrote coee for computing tetragrams (quadgrams)

--- ngramModelTrainer.py
+++ ngramModelTrainer.py
@@ -105,37 +105,79 @@ def computeTrigramsConditionalPdf(trigramsJointPdf, bigramsJointPdf):
             res[anteprevious, previous, :] = res[anteprevious, previous, :] / res[anteprevious, previous, :].flatten().sum()
     return res
 
+def countQuadgrams(word):
+    res = Counter()
+    l = list(word)
+    for i in range(0, len(word)-3):
+        res[(l[i], l[i+1], l[i+2], l[i+3])] += 1
+    return res
+
+def computeQuadgramsJointPdf(quadgrams):
+    # This is the (joint) probability of tetragrams ("quadgrams")
+    res = np.zeros( (len(alphabet), len(alphabet), len(alphabet), len(alphabet)) )
+    for i in range(0, len(alphabet)):
+        for j in range(0, len(alphabet)):
+            for k in range(0, len(alphabet)):
+                for l in range(0, len(alphabet)):
+                    res[i, j, k, l] = quadgrams[alphabet[i], alphabet[j], alphabet[k], alphabet[l]]
+                    if res[i, j, k, l] == 0:
+                        res[i, j, k, l] = np.finfo(float).eps                
+    res = res / res.flatten().sum()
+    return res
+
+def computeQuadgramsConditionalPdf(quadgramsJointPdf, trigramsJointPdf):
+    res = np.zeros( (len(alphabet), len(alphabet), len(alphabet), len(alphabet)) )
+    for previous in range(0, len(alphabet)):
+        for anteprevious in range(0, len(alphabet)):
+            for paranteprevious in range(0, len(alphabet)):
+                for i in range(0, len(alphabet)):
+                    res[paranteprevious, anteprevious, previous, i] = quadgramsJointPdf[paranteprevious, anteprevious, previous, i] / trigramsJointPdf[paranteprevious, anteprevious, previous]
+    #Renormalize (to avoid numerical errors)
+    for previous in range(0, len(alphabet)):
+        for anteprevious in range(0, len(alphabet)):
+            for paranteprevious in range(0, len(alphabet)):
+                res[paranteprevious, anteprevious, previous, :] = res[paranteprevious, anteprevious, previous, :] / res[paranteprevious, anteprevious, previous, :].flatten().sum()
+    return res
+
 def main(filename):
     unigrams = Counter()
     bigrams = Counter()
     trigrams = Counter()
+    quadgrams = Counter()
     for word in fileinput.input(filename):
         strippedword = strip(word)
         unigrams += countUnigrams(strippedword)
         bigrams += countBigrams(strippedword)
         trigrams += countTrigrams(strippedword)
+        quadgrams += countQuadgrams(strippedword)
     unigramsPdf = computeUnigramsPdf(unigrams)
     bigramsJointPdf = computeBigramsJointPdf(bigrams)
     trigramsJointPdf = computeTrigramsJointPdf(trigrams)
-    return unigramsPdf, bigramsJointPdf, trigramsJointPdf    
+    quadgramsJointPdf = computeQuadgramsJointPdf(quadgrams)
+    return unigramsPdf, bigramsJointPdf, trigramsJointPdf, quadgramsJointPdf
 
 class TestMethods(unittest.TestCase):
     def test_unigrams(self):
-        unigramsPdf, b, c = main('fixtures/dummy.txt')
+        unigramsPdf, b, c, _ = main('fixtures/dummy.txt')
         self.assertTrue(abs(unigramsPdf[alphabet.index('a')] - 5./35.) < 1e-5)
         self.assertTrue(abs(unigramsPdf[alphabet.index('z')]) < 1e-5)
 
     def test_bigrams(self):
-        unigramsPdf, bigramsJointPdf, c = main('fixtures/dummy.txt')
+        unigramsPdf, bigramsJointPdf, c, _ = main('fixtures/dummy.txt')
         bigramsPdf = computeBigramsConditionalPdf(bigramsJointPdf, unigramsPdf)
         self.assertTrue(abs(bigramsPdf[alphabet.index('l'), alphabet.index('l')] - 1./4.) < 1e-5)
 
     def test_trigrams(self):
-        unigramsPdf, bigramsJointPdf, trigramsJointPdf = main('fixtures/dummy.txt')
+        unigramsPdf, bigramsJointPdf, trigramsJointPdf, _ = main('fixtures/dummy.txt')
         trigramsPdf = computeTrigramsConditionalPdf(trigramsJointPdf, bigramsJointPdf)
         self.assertTrue(abs(trigramsPdf[alphabet.index('a'), alphabet.index('i'), alphabet.index('n')] - 1.) < 1e-5)
         self.assertTrue(abs(trigramsPdf[alphabet.index('t'), alphabet.index('h'), alphabet.index('e')] - 1.) < 1e-5)
         self.assertTrue(abs(trigramsPdf[alphabet.index('x'), alphabet.index('y'), alphabet.index('z')]) < 2./36.)
+
+    def test_quadgrams(self):
+        unigramsPdf, bigramsJointPdf, trigramsJointPdf, quadgramsJointPdf = main('fixtures/dummy2.txt')
+        quadgramsPdf = computeQuadgramsConditionalPdf(quadgramsJointPdf, trigramsJointPdf)
+        self.assertTrue(abs(quadgramsPdf[alphabet.index('r'), alphabet.index('s'), alphabet.index('o'), alphabet.index('n')] - 1.) < 1e-5)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
@@ -150,13 +192,14 @@ if __name__ == "__main__":
         testMode = True
 
     if not testMode:
-        unigramsPdf, bigramsJointPdf, trigramsJointPdf = main(filename)
+        unigramsPdf, bigramsJointPdf, trigramsJointPdf, quadgramsJointPdf = main(filename)
         #print(np.log(computeBigramsConditionalPdf(bigramsJointPdf, unigramsPdf)))
         #print(np.log(computeTrigramsConditionalPdf(trigramsJointPdf, bigramsJointPdf)))
         scipy.io.savemat(basename(filename)+'.ngrams.mat', dict(
             unigrams=unigramsPdf, 
             bigrams=computeBigramsConditionalPdf(bigramsJointPdf, unigramsPdf),
             trigrams=computeTrigramsConditionalPdf(trigramsJointPdf, bigramsJointPdf),
+            quadgrams=computeQuadgramsConditionalPdf(quadgramsJointPdf, trigramsJointPdf),
             )
         )
     else:
